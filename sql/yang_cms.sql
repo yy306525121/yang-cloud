796ea06df7086f821a084c76694d03fa6c49d066
杨跑跑 <akademos_yang@163.com>
2019-05-21 09:31:07 +0800

    更新SQL

--- sql/yang_cms.sql
+++ sql/yang_cms.sql
@@ -9,5 +9,9 @@ create table t_resource(
   create_time datetime,
   create_uid int(10),
   update_time datetime,
-  update_uid int(10)
+  update_uid int(10),
+  `type` varchar(10),
+  size int(10),
+  status tinyint(1),
+  md5 varchar(16)
 )
